cba46bccf03de478d3fba567118fc7a28b6a8dbc
Mike Cook <mcook@netflix.com>
2018-03-25 15:30:32 -0700

    Initial working version.

---
+++ src/node.js
@@ -0,0 +1,12 @@
+// import Connection from 'connection';
+//
+// export default class Node {
+//     constructor({ position }) {
+//         this.position = position;
+//         this.connections = [];
+//     }
+//
+//     addConnection(nodeToConnect) {
+//         this.connections.push(new Connection(this, nodeToConnect));
+//     }
+// }
